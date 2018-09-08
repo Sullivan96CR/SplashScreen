97a7cedd13163cd12c5192e00ef2f3987fa4b115
Proyecto TCU <jsandoval607@gmail.com>
2018-09-08 12:48:44 -0600

    Probando github

---
+++ app/src/main/java/com/example/dell/splashscreen/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.example.dell.splashscreen;
+
+import android.content.Intent;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageView = (ImageView) findViewById(R.id.ivWelcome);
+        textView = (TextView) findViewById(R.id.tvWelcome);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        textView.startAnimation(anim);
+        imageView.startAnimation(anim);
+
+        final Intent intent = new Intent(this, MainActivity.class);
+        Thread timer = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
